331c2863b5b988abe840717b27f9525cd720ce01
James Mwangi <karokijames40@gmail.com>
2019-05-22 11:17:49 +0300

    Adds iframe

--- resources/views/demo-unhcr.blade.php
+++ resources/views/demo-unhcr.blade.php
@@ -70,13 +70,17 @@
                     #Demo 2
                 </h2>
                 <p>
-                    Refugees are people just like you. The difference is they have been forced to flee their homes.
-                    The numbers of people fleeing their home, their country, has accelerated to a level never seen before. In the past five years no fewer than 15 conflicts – some new, some old – have brought unspeakable tragedy and misery to millions across the world.
-                    UNHCR has responded. We have stretched our resources to help more people than ever before. UNHCR is present in all of the world’s conflict hotspots, working around the clock to extend our protection and lifesaving aid to millions.
-                    Donate NOW to make a difference and help save lives.
+                    In Kenya, thousands of refugee youth in the Dadaab and Kakuma refugee camps are unable to go to school because of the lack of resources and funding. They are sitting idle, feeling hopeless and frustrated.
+
+                    This Ramadan you can change this.
+
+                    Join us as we raise funds to increase access to education for refugee youth.
+
+                    With your donation, you can help them not only better themselves but also one day, inshAllah, return home and better their home countries.
+
                 </p>
                 <h4>
-                    Donate NOW to make a difference and help save lives.
+                    Please give generously today.
                 </h4>
                 <a href="{{ url('demo-donation-page-2') }}" class="btn btn-block btn-success btn-lg">
                     Donate With Mpesa
