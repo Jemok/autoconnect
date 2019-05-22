ad8747f04d344e45a783ececf16d25e44c96ac58
James Mwangi <karokijames40@gmail.com>
2019-05-22 11:32:46 +0300

    Adds iframe

--- resources/views/demo-donation.blade.php
+++ resources/views/demo-donation.blade.php
@@ -51,7 +51,7 @@
         </h2>
         <div class="row justify-content-center" style="margin-top: 4%;">
             {{--<iframe src="https://tujengepay.com/collection-link/c596aa60-7c57-11e9-a91d-93ac315e33cf" style="border:0px #ffffff none;" name="myiFrame" scrolling="no" frameborder="1" marginheight="0px" marginwidth="0px" height="400px" width="600px" allowfullscreen></iframe>--}}
-            <iframe src="https://tujengepay.com/iframe-link/c596aa60-7c57-11e9-a91d-93ac315e33cf" style="border:0px #ffffff none;" name="myiFrame" scrolling="no" frameborder="1" marginheight="0px" marginwidth="0px" height="400px" width="600px" allowfullscreen></iframe>
+            <iframe src="https://tujengepay.com/iframe-link/19744390-7c6c-11e9-bed3-a9090beab398" style="border:0px #ffffff none;" name="myiFrame" scrolling="no" frameborder="1" marginheight="0px" marginwidth="0px" height="400px" width="600px" allowfullscreen></iframe>
         </div>
     </div>
 </div>
