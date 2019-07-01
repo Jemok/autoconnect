e1ed5a4ef75de19391538bc05ee5c2f6e399fc06
James Mwangi <karokijames40@gmail.com>
2019-07-01 07:14:42 +0300

    Update on user interface design

--- resources/views/vehicles/publish-ad.blade.php
+++ resources/views/vehicles/publish-ad.blade.php
@@ -2,9 +2,9 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-8" style="margin-top: 3%;">
                 <div class="card">
-                    <div class="card-header">Your Ad has been created successfully</div>
+                    <div class="card-header background-nav">Your Ad has been created successfully</div>
 
                     <div class="card-body">
                         @if (session('status'))
@@ -17,12 +17,15 @@
                             <div class="col-sm-12">
                                 <div class="card">
                                     <div class="card-body">
+                                        <h4 class="text-center">
+                                            You will receive an email when your ad has been approved and published online to our website
+                                        </h4>
                                         <p class="text-center">
-                                            You will receive an email when your ad has been published
+                                            <i class="fa fa-check text-success" style="font-size: 150px;"></i>
                                         </p>
-                                        <p class="text-center">
+                                        <h5 class="text-center">
                                             Thank you
-                                        </p>
+                                        </h5>
                                     </div>
                                 </div>
                             </div>
