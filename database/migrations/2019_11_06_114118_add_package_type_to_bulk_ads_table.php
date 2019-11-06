00b0c0f232c0bcbb05c393c80fd62e9c30a81597
James Mwangi <karokijames40@gmail.com>
2019-11-06 14:54:39 +0300

    Chnages

--- database/migrations/2019_11_06_114118_add_package_type_to_bulk_ads_table.php
+++ database/migrations/2019_11_06_114118_add_package_type_to_bulk_ads_table.php
@@ -14,7 +14,7 @@ class AddPackageTypeToBulkAdsTable extends Migration
     public function up()
     {
         Schema::table('bulk_ads', function (Blueprint $table) {
-            $table->string('type');
+            $table->string('type')->nullable();
         });
     }
 
