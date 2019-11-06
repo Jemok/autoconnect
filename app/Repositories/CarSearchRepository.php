d4d14feeb53542e673c97ba866bf8ceab5e7144c
James Mwangi <karokijames40@gmail.com>
2019-11-06 12:00:14 +0300

    Chnages

--- app/Repositories/CarSearchRepository.php
+++ app/Repositories/CarSearchRepository.php
@@ -89,6 +89,8 @@ class CarSearchRepository
 
             }
 
+            dd($raw_vehicles->get());
+
             $vehicles = $raw_vehicles->orWhereBetween('year', [$year_from, $year_to])
                 ->orWhereBetween('price', [$min_price, $max_price])
                 ->orWhere('body_type_id', $body_type_id)
