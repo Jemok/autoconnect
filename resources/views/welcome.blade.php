a0d009a2eb31424c5229ea8ba7df556735b9094c
James <karokijames40@gmail.com>
2020-07-10 16:35:16 +0300

    Changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -545,6 +545,14 @@
                                 </div>
                             @endforeach
                         </div>
+                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
                     </div>
 
                     <div class="row" style="padding-left: 0px; margin-top: 1%">
